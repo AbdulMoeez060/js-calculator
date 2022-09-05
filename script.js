ef82a3f941fcfe4ce1768f8af39b2e76ceb05784
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-09-05 20:43:17 +0500

    add delete num func

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ var currNum="",prevNum="",operator="";
 
 const digits = document.querySelectorAll('[data-num]')
 const mainNum = document.querySelector('.lower');
+const del = document.querySelector('[data-del]');
+
 
 
 document.addEventListener('keydown',getNum)
+document.addEventListener('keydown',delDigit)
+
+
 digits.forEach(digit => {
     digit.addEventListener('click',getNum)
 
 })
+
+del.addEventListener('click',delDigit);
 
 function getNum(e){
     if (e instanceof KeyboardEvent && (e.key>=0 && e.key <=9)) {
@@ -24,4 +31,14 @@ function getNum(e){
 
 function updateMainNum(newNum){
     mainNum.textContent = newNum;
+}
+
+function delDigit(e){
+    if ((e instanceof KeyboardEvent && e.key==='Backspace')||e instanceof PointerEvent) {
+        if (currNum.length>0) {
+            currNum = currNum.slice(0,-1)
+            updateMainNum(currNum)
+            
+        }
+    }
 }
